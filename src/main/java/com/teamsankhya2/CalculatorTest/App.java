14c4572b809697a7e9eafbe6c2bbfe58383456d4
Naveen Kumar R <naveensurya207@gmail.com>
2018-09-06 12:12:33 +0530

    commit

---
+++ src/main/java/com/teamsankhya2/CalculatorTest/App.java
@@ -0,0 +1,45 @@
+package com.teamsankhya2.CalculatorTest;
+
+import com.teamsankhya.Calculator.Add;
+import com.teamsankhya.Calculator.Division;
+import com.teamsankhya.Calculator.Multiply;
+import com.teamsankhya.Calculator.Subtract;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int sum=Add.add(10,30);
+        System.out.println("sum : "+sum);
+        
+//        double sum1=Add.add(1.0,3.2);
+//        System.out.println("sum1 : "+sum1);
+//        
+//        long sum2=Add.add(11111111,111111111);
+//        System.out.println("sum2 : "+sum2);
+//        
+//        int pro=Multiply.product(10,30);
+//        System.out.println("product : "+pro);
+//        
+//        double pro1=Multiply.product(10.7,3.20);
+//        System.out.println("product : "+pro1);
+//        
+//        int sub=Subtract. subtract(30,10);
+//        System.out.println("sub : "+ sub );
+//        
+//        double sub1=Subtract.subtract(1.3, 2.2);
+//        System.out.println("sub1 : "+ sub );
+//        
+//        double div=Division.divide(10,30);
+//        System.out.println("division : "+div);
+//        
+//        float div1=Division.divide(18f,9f);
+//        System.out.println("division : "+div1);
+        
+        
+    }
+}
